821c3e698eaec3e2c801b91bec8df981a6b420af
LeZi <bb20048862@gmail.com>
2023-10-26 00:29:38 +0800

    Add files via upload

---
+++ Calculator/Main.Designer.cs
@@ -0,0 +1,300 @@
+﻿namespace Calculator
+{
+    partial class Main
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Ouput = new System.Windows.Forms.TextBox();
+            this.Num1 = new System.Windows.Forms.Button();
+            this.Num2 = new System.Windows.Forms.Button();
+            this.Num3 = new System.Windows.Forms.Button();
+            this.Num4 = new System.Windows.Forms.Button();
+            this.Num5 = new System.Windows.Forms.Button();
+            this.Num6 = new System.Windows.Forms.Button();
+            this.Num7 = new System.Windows.Forms.Button();
+            this.Num8 = new System.Windows.Forms.Button();
+            this.Num9 = new System.Windows.Forms.Button();
+            this.Num0 = new System.Windows.Forms.Button();
+            this.Point = new System.Windows.Forms.Button();
+            this.AllClean = new System.Windows.Forms.Button();
+            this.del = new System.Windows.Forms.Button();
+            this.MemoryAdd = new System.Windows.Forms.Button();
+            this.MemoryDel = new System.Windows.Forms.Button();
+            this.RCMemory = new System.Windows.Forms.Button();
+            this.Total = new System.Windows.Forms.Button();
+            this.Status = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Ouput
+            // 
+            this.Ouput.BackColor = System.Drawing.SystemColors.Control;
+            this.Ouput.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Ouput.Font = new System.Drawing.Font("Microsoft YaHei UI", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Ouput.Location = new System.Drawing.Point(12, 55);
+            this.Ouput.Name = "Ouput";
+            this.Ouput.Size = new System.Drawing.Size(354, 37);
+            this.Ouput.TabIndex = 0;
+            this.Ouput.Text = "0";
+            this.Ouput.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // Num1
+            // 
+            this.Num1.Font = new System.Drawing.Font("Microsoft YaHei UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Num1.Location = new System.Drawing.Point(40, 140);
+            this.Num1.Name = "Num1";
+            this.Num1.Size = new System.Drawing.Size(77, 36);
+            this.Num1.TabIndex = 1;
+            this.Num1.Text = "1";
+            this.Num1.UseVisualStyleBackColor = true;
+            // 
+            // Num2
+            // 
+            this.Num2.Font = new System.Drawing.Font("Microsoft YaHei UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Num2.Location = new System.Drawing.Point(123, 140);
+            this.Num2.Name = "Num2";
+            this.Num2.Size = new System.Drawing.Size(77, 36);
+            this.Num2.TabIndex = 2;
+            this.Num2.Text = "2";
+            this.Num2.UseVisualStyleBackColor = true;
+            // 
+            // Num3
+            // 
+            this.Num3.Font = new System.Drawing.Font("Microsoft YaHei UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Num3.Location = new System.Drawing.Point(206, 140);
+            this.Num3.Name = "Num3";
+            this.Num3.Size = new System.Drawing.Size(77, 36);
+            this.Num3.TabIndex = 3;
+            this.Num3.Text = "3";
+            this.Num3.UseVisualStyleBackColor = true;
+            // 
+            // Num4
+            // 
+            this.Num4.Font = new System.Drawing.Font("Microsoft YaHei UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Num4.Location = new System.Drawing.Point(40, 182);
+            this.Num4.Name = "Num4";
+            this.Num4.Size = new System.Drawing.Size(77, 36);
+            this.Num4.TabIndex = 4;
+            this.Num4.Text = "4";
+            this.Num4.UseVisualStyleBackColor = true;
+            // 
+            // Num5
+            // 
+            this.Num5.Font = new System.Drawing.Font("Microsoft YaHei UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Num5.Location = new System.Drawing.Point(123, 182);
+            this.Num5.Name = "Num5";
+            this.Num5.Size = new System.Drawing.Size(77, 36);
+            this.Num5.TabIndex = 5;
+            this.Num5.Text = "5";
+            this.Num5.UseVisualStyleBackColor = true;
+            // 
+            // Num6
+            // 
+            this.Num6.Font = new System.Drawing.Font("Microsoft YaHei UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Num6.Location = new System.Drawing.Point(206, 182);
+            this.Num6.Name = "Num6";
+            this.Num6.Size = new System.Drawing.Size(77, 36);
+            this.Num6.TabIndex = 6;
+            this.Num6.Text = "6";
+            this.Num6.UseVisualStyleBackColor = true;
+            // 
+            // Num7
+            // 
+            this.Num7.Font = new System.Drawing.Font("Microsoft YaHei UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Num7.Location = new System.Drawing.Point(40, 224);
+            this.Num7.Name = "Num7";
+            this.Num7.Size = new System.Drawing.Size(77, 36);
+            this.Num7.TabIndex = 7;
+            this.Num7.Text = "7";
+            this.Num7.UseVisualStyleBackColor = true;
+            // 
+            // Num8
+            // 
+            this.Num8.Font = new System.Drawing.Font("Microsoft YaHei UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Num8.Location = new System.Drawing.Point(123, 224);
+            this.Num8.Name = "Num8";
+            this.Num8.Size = new System.Drawing.Size(77, 36);
+            this.Num8.TabIndex = 8;
+            this.Num8.Text = "8";
+            this.Num8.UseVisualStyleBackColor = true;
+            // 
+            // Num9
+            // 
+            this.Num9.Font = new System.Drawing.Font("Microsoft YaHei UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Num9.Location = new System.Drawing.Point(206, 224);
+            this.Num9.Name = "Num9";
+            this.Num9.Size = new System.Drawing.Size(77, 36);
+            this.Num9.TabIndex = 9;
+            this.Num9.Text = "9";
+            this.Num9.UseVisualStyleBackColor = true;
+            // 
+            // Num0
+            // 
+            this.Num0.Font = new System.Drawing.Font("Microsoft YaHei UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Num0.Location = new System.Drawing.Point(123, 266);
+            this.Num0.Name = "Num0";
+            this.Num0.Size = new System.Drawing.Size(77, 36);
+            this.Num0.TabIndex = 10;
+            this.Num0.Text = "0";
+            this.Num0.UseVisualStyleBackColor = true;
+            // 
+            // Point
+            // 
+            this.Point.Font = new System.Drawing.Font("Microsoft YaHei UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Point.Location = new System.Drawing.Point(206, 266);
+            this.Point.Name = "Point";
+            this.Point.Size = new System.Drawing.Size(77, 36);
+            this.Point.TabIndex = 11;
+            this.Point.Text = ".";
+            this.Point.UseVisualStyleBackColor = true;
+            // 
+            // AllClean
+            // 
+            this.AllClean.Font = new System.Drawing.Font("Microsoft YaHei UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.AllClean.Location = new System.Drawing.Point(289, 98);
+            this.AllClean.Name = "AllClean";
+            this.AllClean.Size = new System.Drawing.Size(77, 36);
+            this.AllClean.TabIndex = 12;
+            this.AllClean.Text = "AC";
+            this.AllClean.UseVisualStyleBackColor = true;
+            // 
+            // del
+            // 
+            this.del.Font = new System.Drawing.Font("Microsoft YaHei UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.del.Location = new System.Drawing.Point(206, 98);
+            this.del.Name = "del";
+            this.del.Size = new System.Drawing.Size(77, 36);
+            this.del.TabIndex = 13;
+            this.del.Text = "DEL";
+            this.del.UseVisualStyleBackColor = true;
+            // 
+            // MemoryAdd
+            // 
+            this.MemoryAdd.Font = new System.Drawing.Font("Microsoft YaHei UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.MemoryAdd.Location = new System.Drawing.Point(289, 182);
+            this.MemoryAdd.Name = "MemoryAdd";
+            this.MemoryAdd.Size = new System.Drawing.Size(77, 36);
+            this.MemoryAdd.TabIndex = 14;
+            this.MemoryAdd.Text = "M+";
+            this.MemoryAdd.UseVisualStyleBackColor = true;
+            // 
+            // MemoryDel
+            // 
+            this.MemoryDel.Font = new System.Drawing.Font("Microsoft YaHei UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.MemoryDel.Location = new System.Drawing.Point(289, 224);
+            this.MemoryDel.Name = "MemoryDel";
+            this.MemoryDel.Size = new System.Drawing.Size(77, 36);
+            this.MemoryDel.TabIndex = 15;
+            this.MemoryDel.Text = "M-";
+            this.MemoryDel.UseVisualStyleBackColor = true;
+            // 
+            // RCMemory
+            // 
+            this.RCMemory.Font = new System.Drawing.Font("Microsoft YaHei UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.RCMemory.Location = new System.Drawing.Point(289, 266);
+            this.RCMemory.Name = "RCMemory";
+            this.RCMemory.Size = new System.Drawing.Size(77, 36);
+            this.RCMemory.TabIndex = 16;
+            this.RCMemory.Text = "MR";
+            this.RCMemory.UseVisualStyleBackColor = true;
+            // 
+            // Total
+            // 
+            this.Total.Font = new System.Drawing.Font("Microsoft YaHei UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Total.Location = new System.Drawing.Point(289, 140);
+            this.Total.Name = "Total";
+            this.Total.Size = new System.Drawing.Size(77, 36);
+            this.Total.TabIndex = 17;
+            this.Total.Text = "GT";
+            this.Total.UseVisualStyleBackColor = true;
+            // 
+            // Status
+            // 
+            this.Status.BackColor = System.Drawing.SystemColors.Control;
+            this.Status.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Status.Font = new System.Drawing.Font("Microsoft YaHei UI", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Status.Location = new System.Drawing.Point(12, 12);
+            this.Status.Name = "Status";
+            this.Status.Size = new System.Drawing.Size(354, 37);
+            this.Status.TabIndex = 18;
+            this.Status.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(402, 327);
+            this.Controls.Add(this.Status);
+            this.Controls.Add(this.Total);
+            this.Controls.Add(this.RCMemory);
+            this.Controls.Add(this.MemoryDel);
+            this.Controls.Add(this.MemoryAdd);
+            this.Controls.Add(this.del);
+            this.Controls.Add(this.AllClean);
+            this.Controls.Add(this.Point);
+            this.Controls.Add(this.Num0);
+            this.Controls.Add(this.Num9);
+            this.Controls.Add(this.Num8);
+            this.Controls.Add(this.Num7);
+            this.Controls.Add(this.Num6);
+            this.Controls.Add(this.Num5);
+            this.Controls.Add(this.Num4);
+            this.Controls.Add(this.Num3);
+            this.Controls.Add(this.Num2);
+            this.Controls.Add(this.Num1);
+            this.Controls.Add(this.Ouput);
+            this.DoubleBuffered = true;
+            this.Name = "Main";
+            this.Text = "Calculator";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Ouput;
+        private System.Windows.Forms.Button Num1;
+        private System.Windows.Forms.Button Num2;
+        private System.Windows.Forms.Button Num3;
+        private System.Windows.Forms.Button Num4;
+        private System.Windows.Forms.Button Num5;
+        private System.Windows.Forms.Button Num6;
+        private System.Windows.Forms.Button Num7;
+        private System.Windows.Forms.Button Num8;
+        private System.Windows.Forms.Button Num9;
+        private System.Windows.Forms.Button Num0;
+        private System.Windows.Forms.Button Point;
+        private System.Windows.Forms.Button AllClean;
+        private System.Windows.Forms.Button del;
+        private System.Windows.Forms.Button MemoryAdd;
+        private System.Windows.Forms.Button MemoryDel;
+        private System.Windows.Forms.Button RCMemory;
+        private System.Windows.Forms.Button Total;
+        private System.Windows.Forms.TextBox Status;
+    }
+}
